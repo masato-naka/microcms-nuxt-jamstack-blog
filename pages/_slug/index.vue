b2350f356742524a4918642d36950b19c1e7060f
Masato Nakamura <masato.nakamura@unifa-e.com>
2021-01-21 17:44:25 +0900

    tailwind css add

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,8 +1,30 @@
 <template>
   <main class="main">
-    <h1 class="title">{{ title }}</h1>
+    <h1 class="title bg-gray-300 hover:bg-gray-400">{{ title }}</h1>
     <p class="publishedAt">{{ publishedAt }}</p>
     <div class="post" v-html="body"></div>
+    <div class="todo">
+      <h2 class="mb-2 px-2 text-xl">New Todo</h2>
+      <form class="mb-4 px-4">
+        <input type="text" class="p-2 border" />
+        <button class="ml-2 p-2 rounded text-white bg-blue-500">submit</button>
+      </form>
+      <h2 class="mb-2 px-2 text-xl">List Todo</h2>
+      <ul class="py-2 px-4">
+        <li class="p-2 border">
+          <p class="border-b">todo 1</p>
+          <p class="text-sm">2020 05/12</p>
+        </li>
+        <li class="mt-2 p-2 border">
+          <p class="border-b">todo 2</p>
+          <p class="text-sm">2020 05/12</p>
+        </li>
+        <li class="mt-2 p-2 border">
+          <p class="border-b">todo 3</p>
+          <p class="text-sm">2020 05/12</p>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
 
